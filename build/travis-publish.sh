5ddb3a9bde874524e923d7200f6ef8efe730d148
Attila M. Magyar <attila.m.magyar@gmail.com>
2014-03-23 23:21:44 +0100

    refine Travis-CI integration

--- build/travis-publish.sh
+++ build/travis-publish.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Based on
+# http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+PROJECT_NAME="attilammagyar/html-filter"
+
 echo "Build report publisher:"
+echo " - TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER"
 echo " - TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo " - TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
 echo " - TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 
-[[ "$TRAVIS_REPO_SLUG" == "attilammagyar/html-filter" ]] || exit 0
+
+[[ "$TRAVIS_REPO_SLUG" == "$PROJECT_NAME" ]] || exit 0
 [[ "$TRAVIS_PULL_REQUEST" == "false" ]] || exit 0
 [[ "$TRAVIS_BRANCH" == "master" ]] || exit 0
 
@@ -18,15 +25,15 @@ cp -R docs "$HOME"/docs
 
 git clone --quiet \
           --branch=gh-pages \
-          "https://${GHTOKEN}@github.com/attilammagyar/html-filter" \
-          "$HOME"/gh-pages
+          "https://${GHTOKEN}@github.com/$PROJECT_NAME" \
+          "$HOME"/gh-pages >/dev/null 2>/dev/null
 
 rm -rf "$HOME"/gh-pages/{api,reports,index.html}
 mv "$HOME"/docs/* "$HOME"/gh-pages/
 
 cd "$HOME"/gh-pages/
-git add -f api reports index.html
-git commit -m "Publishing build report: '$TRAVIS_BUILD_NUMBER'"
-git push -fq origin gh-pages
+git add -f . >/dev/null 2>/dev/null
+git commit -m "Publishing build report: '$TRAVIS_BUILD_NUMBER'" >/dev/null 2>/dev/null
+git push -fq origin gh-pages >/dev/null 2>/dev/null
 
-echo "Published build report."
+echo "Finished publishing build report."
